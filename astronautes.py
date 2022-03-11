cdde34db2ada1d628ecd2e5d49927a6bf8e0c63e
marionlmr <marionlemer.2000@gmail.com>
2022-03-11 10:02:56 +0100

    seulement les austronautes de l'ISS

--- astronautes.py
+++ astronautes.py
@@ -2,4 +2,9 @@ import requests
 
 contenu = requests.get("http://api.open-notify.org/astros.json")
 for personne in contenu.json()["people"]:
-    print(personne)
+    if personne["craft"]=="ISS":
+        print(personne)
+
+
+
+
